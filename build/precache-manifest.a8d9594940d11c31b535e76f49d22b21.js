44c65309e59f5f488149e262ebcd524c7b8377d1
Simon <simonxd@gmail.com>
2020-05-09 20:09:30 -0300

    With build

---
+++ build/precache-manifest.a8d9594940d11c31b535e76f49d22b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623bd9914e60a907ca1e25b48ad9efaa",
+    "url": "/turquia-2019/index.html"
+  },
+  {
+    "revision": "502b2912f6e2d0b570fa",
+    "url": "/turquia-2019/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "dd27bb8f737ba8e94fc7",
+    "url": "/turquia-2019/static/css/main.8910beef.chunk.css"
+  },
+  {
+    "revision": "502b2912f6e2d0b570fa",
+    "url": "/turquia-2019/static/js/2.237e30b5.chunk.js"
+  },
+  {
+    "revision": "284d116f86c8eb92a654f6318b2938d9",
+    "url": "/turquia-2019/static/js/2.237e30b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd27bb8f737ba8e94fc7",
+    "url": "/turquia-2019/static/js/main.ef2c364a.chunk.js"
+  },
+  {
+    "revision": "f73c63af79733b715681",
+    "url": "/turquia-2019/static/js/runtime-main.07152e54.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+const path = require('path')
 const browserSync = require('browser-sync')
 
 const server = browserSync.create()
 
 server.init({
-  proxy: 'https://simonfan.com.br/turquia-2019/',
+  // proxy: 'https://simonfan.com.br/turquia-2019/',
+  server: path.join(__dirname, '../build'),
   injectChanges: true,
-  port: process.env.PORT,
+  port: process.env.PORT || 4000,
   open: false,
   ui: false
 })
